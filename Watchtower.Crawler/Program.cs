fa0c0b41fd12cdc61dc956658f760927c60c8563
Michal Jan Warecki <mwarecki1@gmail.com>
2022-04-30 14:54:22 +0200

    Basic file structure.

--- Watchtower.Crawler/Program.cs
+++ Watchtower.Crawler/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Watchtower.Data;
+
+// See https://aka.ms/new-console-template for more information
+static void main() {
+    Console.WriteLine("Hello, World!");
+
+    // Find last request
+    // Return if too short time has passed since last execution
+    // Get hosts
+    // For every host, get resources
+    // For every resource:
+    // - ping it
+    // - upload the result
+}
+
+main();
