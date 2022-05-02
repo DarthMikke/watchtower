08c9139d06da64a0dc4942dee177416e32a0b420
Michal Jan Warecki <mwarecki1@gmail.com>
2022-05-02 15:32:00 +0200

    Embed the app in class Program.

--- Watchtower.Crawler/Program.cs
+++ Watchtower.Crawler/Program.cs
@@ -2,41 +2,52 @@
 using Watchtower.Data;
 using Watchtower.Crawler;
 
-// Fetch credentials
-// Get current directory
-var currentDirectory = System.IO.Directory.GetCurrentDirectory();
-var filename = $"{currentDirectory}/mongodb.txt";
+class Program {
+    static void Main(string[] args) {
+        // Fetch credentials
+        // Get current directory
+        var currentDirectory = System.IO.Directory.GetCurrentDirectory();
+        var filename = $"{currentDirectory}/mongodb.txt";
 
-Console.WriteLine($"Looking for credentials in {filename}...");
+        Console.WriteLine($"Looking for credentials in {filename}...");
 
-if (!System.IO.File.Exists(filename)) {
-    Console.WriteLine("No credentials file was found. Exiting.");
-    System.Environment.Exit(-1);
-}
+        if (!System.IO.File.Exists(filename)) {
+            Console.WriteLine("No credentials file was found. Exiting.");
+            System.Environment.Exit(-1);
+        }
 
-string connectionString = System.IO.File.ReadAllText(filename);
+        string connectionString = System.IO.File.ReadAllText(filename);
 
-var crawler = new Crawler(connectionString);
+        var crawler = new Crawler(connectionString);
 
-// Get hosts
-crawler.LoadHosts();
-// For every host, get resources
-var hostCount = crawler.hosts.Count();
-Console.WriteLine($"Found {hostCount} {(hostCount == 1 ? "host" : "hosts")}.");
-foreach (var host in crawler.hosts)
-{
-    var resources = host.Resources(crawler.client);
-    var resourceCount = resources.Count();
-    Console.WriteLine($"Found {resourceCount} resource{(resourceCount == 1 ? "" : "s")} @ {host.hostname} .");
-    // For every resource:
-    // - Find last request
+        // Get hosts
+        crawler.LoadHosts();
+        // For every host, get resources
+        var hostCount = crawler.hosts.Count();
+        Console.WriteLine($"Found {hostCount} {(hostCount == 1 ? "host" : "hosts")}.");
+        foreach (var host in crawler.hosts)
+        {
+            var resources = host.Resources(crawler.client);
+            var resourceCount = resources.Count();
+            Console.WriteLine($"Found {resourceCount} resource{(resourceCount == 1 ? "" : "s")} @ {host.hostname} .");
+            // For every resource:
+            // - Find last request
 
-    foreach (var resource in resources)
-    {
-        Console.Write($"{resource.method} {host.hostname}{resource.path}... ");
-        Console.WriteLine("Polling.");
+            foreach (var resource in resources)
+            {
+                Console.Write($"{resource.method} {host.hostname}{resource.path}... ");
+                /*bool canPoll = crawler.canCrawl(resource);
+
+                if (!canPoll)
+                {
+                    Console.WriteLine("Polled recently.");
+                    continue;
+                }*/
+                Console.WriteLine("Polling.");
+            }
+            // - Return if too short time has passed since last execution
+            // - ping it
+            // - upload the result
+        }
     }
-    // - Return if too short time has passed since last execution
-    // - ping it
-    // - upload the result
 }
